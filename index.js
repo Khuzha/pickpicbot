7b1486d234ce07dc455fc93336cade4d9ae5c7c9
Sardor <34943401+Khuzha@users.noreply.github.com>
2019-06-30 01:56:29 +0500

    take out functions to sprecial file (picMaker.js)

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Telegraf = require('telegraf')
 const mongo = require('mongodb').MongoClient
 const axios = require('axios')
 const data = require('./data')
+const { value, inline, random, get, start } = require('./picMaker.js')
 
 const bot = new Telegraf(data.token)
 const db = {}
@@ -31,81 +32,8 @@ mongo.connect(data.url, { useNewUrlParser: true }), (error, client) => {
 	bot.launch({})
 })
 
-async function value (ctx) {
-	await ctx.replyWithChatAction('typing')
-	const check = ctx.message.text.split('*')
-	if ([1, 2].includes(check.length)) {
-		let x = Math.round(check[0])
-		let y = Math.round(check[1])
-		if(Math.abs(x-y) > 190 && (x < 30 || y < 30)) {
-			return ctx.reply('Invalid size. Please change resolution')
-		} else {
-			const url = await get(x, y)
-			const keys = {inline_keyboard: [[{ text: '⤴️Share', switch_inline_query: url }]]}
-			
-			await ctx.reply('Searching...')
-			await ctx.replyWithChatAction('upload_photo')
-			
-			if (!y) y = x
 
-			if (!url) {
-				return ctx.reply('Picture not found. Try again')
-			}
-			
-			return ctx.replyWithPhoto('https://picsum.photos/id/' + url, { reply_markup: keys })
-		}
-	} else {
-		return ctx.reply('Invalid query. Try again')
-	}
-}
-
-async function inline (ctx) {
-	const query = ctx.update.inline_query.query
-	const checkPic = query.split('/')
-	if(checkPic.length === 3) {
-		return await bot.telegram.answerInlineQuery(ctx.update.inline_query.id, [{type: 'photo', id: checkPic[0], photo_url: 'https://picsum.photos/id/' + query, thumb_url: 'https://picsum.photos/id/' + query}])
-	}
-	return await bot.telegram.answerInlineQuery(ctx.update.inline_query.id, [])
-}
-
-async function random (ctx) {
-	const x = Math.floor(Math.random() * 4096) + 1
-	const y = Math.floor(Math.random() * 4096) + 1
-	const url = await get(x, y, true)
-	const keys = {inline_keyboard: [[{ text: '⤴️Share', switch_inline_query: url }]]}
-	
-	await ctx.replyWithChatAction('typing')
-	await ctx.reply('Searching...')
-	await ctx.replyWithChatAction('upload_photo')
-	
-	return ctx.replyWithPhoto('https://picsum.photos/id/' + url, { reply_markup: keys })
-}
-
-async function get (x, y, force = false) {
-	let res = ''
-	let url = 'https://picsum.photos/' + x + '/' + y
-	try {
-		res = await axios(url)
-		url = res.request.res.responseUrl.split('id/')[1]
-	} catch (e) {
-		if(force) {
-			url = await get(x, y, true)
-		} else {
-			sendError()
-		}
-	}
-	return url
-}
-
-async function start (ctx) {
-	const keys = { keyboard: [['Get Random Picture'], ['More Bots', 'Statistics']], resize_keyboard: true }
-	return ctx.reply(
-		'Hello\n\nJust send me size of picture\n\nExample:\n```200*300\n1000*400\n1920*1080\n100\n200\n20000```',
-		{ parse_mode: 'markdown', reply_markup: keys }
-	)
-}
-
-sendError = (err, ctx) => {
+function sendError (err, ctx) {
   if (!ctx) {
     return bot.telegram.sendMessage(data.devId, err)
   }
