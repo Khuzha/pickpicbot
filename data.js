147fd969c07d5b4fd8230cd9b260042f2372fde3
Sardor <34943401+Khuzha@users.noreply.github.com>
2019-06-30 01:33:53 +0500

    add token

--- data.js
+++ data.js
@@ -1,4 +1,5 @@
 module.exports = {
+  token: '123...:asdajs...', // string, can get it in @BotFather in the Telegram
   mongoURL: 'string', // can get it on cloud.mongodb.com
   devId: 123 // developer`s id in the Telegram
 }
